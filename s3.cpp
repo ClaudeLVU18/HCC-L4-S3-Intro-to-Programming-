7f4046262640d35053c7c348163338eaf7c0301e
ClaudeLVU18 <84764547+ClaudeLVU18@users.noreply.github.com>
2022-01-11 02:19:22 +0400

    Create s3.cpp

---
+++ s3.cpp
@@ -0,0 +1,244 @@
+#include <iostream> //allows for variables to be loaded.
+#include <string> // allows string to be used.
+#include <stdlib.h> // allows for "system("CLS")" to be used to clear the console.
+#include <array> // allows for array functions.
+
+
+
+using namespace std;
+
+string s3type(char a)
+{
+    switch (a)
+    {
+    case 'a':
+    case 'A':
+        return "Ice";
+        break;
+    case 'b':
+    case 'B':
+        return "Milk";
+        break;
+    case 'C':
+    case 'c':
+        return "Black";
+        break;
+    }
+}
+
+string s3drink(char b)
+{
+    if ((b = 'A')||(b = 'a'))
+    {
+        return "Coffee";
+    }
+    else
+    {
+        return "Tea";
+    }
+    
+}
+
+string s3sugar(char yesno)
+{
+    
+    switch (yesno)
+    {
+    case 'y':
+    case 'Y':
+        return "with extra sugar";
+        break;
+    case 'n':
+    case 'N':
+        return "without extra sugar";
+        break;
+    }
+    
+}
+
+int s3price(char c)
+{
+    switch (c)
+    {
+    case 'a':
+    case 'A':
+        return 3;
+        break;
+    case 'b':
+    case 'B':
+        return 2;
+        break;
+    case 'C':
+    case 'c':
+        return 1;
+        break;
+    }
+}
+
+int main()
+{
+    bool end = true;
+    while(end){
+    system("CLS");
+     string menu[4][4] = {
+
+        {"Coffee", "          Price (AED)", "    Tea", "          Price (AED)"},
+        {"Ice Coffee", "      3", "              Ice Tea", "      3"},
+        {"Milk Coffee", "     2", "              Milk Tea", "     2"},
+        {"Black Coffee", "    1", "              Black Tea", "    1"}
+    };
+
+    for(int i = 0; i<4; i++){
+        for(int j = 0; j <4; j++){
+            cout << menu[i][j] << " ";
+        }
+        cout << endl;
+    }
+    
+    cout << "\n\nPlease insert amount of coins or cash (Minimum: 3 AED): ";
+    double wallet;
+    cin >> wallet;
+    while(cin.fail()){
+         
+        cout <<"Error: invalid amount.\n ";
+        cout << "\n\nPlease insert amount of coins or cash: ";
+
+        cin.clear();
+        cin.ignore(1000, '\n');
+        cin >> wallet;
+    }
+    while(wallet<3){
+          
+        cout <<"Error: Minimum: 3 AED.\n ";
+        cout << "\n\nPlease insert amount of coins or cash: ";
+
+        cin.clear();
+        cin.ignore(1000, '\n');
+        cin >> wallet;
+    }
+
+    bool wrongOrder = true;
+    while (wrongOrder)
+    {
+     system("CLS");   
+     string menu2[4][4] = {
+
+        {"Coffee", "          Price (AED)", "    Tea", "          Price (AED)"},
+        {"Ice Coffee", "      3", "              Ice Tea", "      3"},
+        {"Milk Coffee", "     2", "              Milk Tea", "     2"},
+        {"Black Coffee", "    1", "              Black Tea", "    1"}
+    };
+
+    for(int i = 0; i<4; i++){
+        for(int j = 0; j <4; j++){
+            cout << menu2[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    cout<<"\nBALANCE: "<<wallet<<" AED"<<endl;
+    cout<<"\nA.Coffee\nB.Tea";
+    cout<< "\nPlease select the drink of your choice: ";
+    
+    char drink;
+    cin >>drink;
+    while((drink != tolower('a'))&&(drink != tolower('b'))){
+         
+        cout <<"Error: invalid selection.\n ";
+        cout<< "\nPlease select the drink of your choice: ";
+
+        cin.clear();
+        cin.ignore(1000, '\n');
+        cin >> drink;
+    }
+    cout<<"\nA.Ice\nB.Milk\nC.Black";
+    cout<< "\nPlease select the drink type of your choice: ";
+    char type;
+    cin >>type;
+    while((type != tolower('a'))&&(type != tolower('b'))&&(type != towlower('c'))){
+         
+        cout <<"Error: invalid selection.\n ";
+        cout<< "\nPlease select the drink type of your choice: ";
+
+        cin.clear();
+        cin.ignore(1000, '\n');
+        cin >> type;
+    }
+ 
+    cout <<"\nWould you like to have some sugar?"<<endl;
+    cout<<"Y/N: ";
+    char sugar;
+    cin>>sugar;
+    while((sugar != tolower('y'))&&(sugar != tolower('n'))){
+         
+        cout <<"Error: kindly input Y or N: ";
+    
+
+        cin.clear();
+        cin.ignore(1000, '\n');
+        cin >> sugar;
+    }
+    
+    
+
+    cout << "\n\nYou have selected: "<< s3type(type) <<" "<< s3drink(drink) <<" "<<s3sugar(sugar)<< ". That would be: "<< s3price(type)<<" AED"<<".\nProceed?";
+    cout<<"\nY/N: ";
+    char userInput;
+    cin >> userInput;
+    while((userInput != tolower('y'))&&(userInput != tolower('n'))){
+         
+        cout <<"Error: kindly input Y or N: ";
+    
+
+        cin.clear();
+        cin.ignore(1000, '\n');
+        cin >> userInput;
+    }
+    switch (userInput)
+    {
+    case 'y':
+    case 'Y':
+        wrongOrder = !true;
+        break;
+    }
+    cout<< "Your change is: "<< wallet - s3price(type);
+    }
+    
+    cout<<"\nWould you like to make another order?";
+    cout<<"\nY/N:";
+    char newOrder;
+    cin >> newOrder;
+    while((newOrder != tolower('y'))&&(newOrder != tolower('n'))){
+         
+        cout <<"Error: kindly input Y or N: ";
+    
+
+        cin.clear();
+        cin.ignore(1000, '\n');
+        cin >> newOrder;
+    }
+    switch (newOrder)
+    {
+    case 'N':
+    case 'n':
+        end = !true;
+        system("CLS");
+        cout<<"Thank you for using this vending machine.";
+        break;
+    }
+    
+    
+    
+    
+    }
+
+    
+    
+
+
+
+
+   
+
+    return 0;
+}
